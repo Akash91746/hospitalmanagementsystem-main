1361384a36f61aae79793e512ee9458af82865e8
Akash91746 <sahaniakash91@gmail.com>
2022-04-21 19:00:10 +0530

    updated settings.py

--- HospitalManagementSystem/settings.py
+++ HospitalManagementSystem/settings.py
@@ -1,3 +1,4 @@
+import os
 """
 Django settings for HospitalManagementSystem project.
 
@@ -119,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
